b3ac8cc1fec1f10bf4c2d03e4e0b2517dee67e9a
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2025-09-11 16:48:32 -0500

    Updated README.md file

---
+++ README.md
@@ -0,0 +1,20 @@
+# Intermediate Software Development Automated Teller Project
+This project will be developed over the course of several assignments.  Each assignment will build on the work done in the previous assignment(s).  Ultimately, an entire system will be created to manage bank transactions for clients who have one or more bank accounts.
+
+## Author
+Komalpreet Kaur
+
+## Assignment
+Assignment 1: Project Setup
+The main tasks included in this assignment are:
+- Downloading and managing project file(isd_project)
+- Initializing a Git Repository
+- Creating Remote Repository in GitHub
+- Adding instructor as collaborator
+- Connecting Local Repository to Remote Repository
+- Launching VS code
+- Updating Remote Repository
+- Installing email-validator
+
+## Encapsulation
+[use this section to explain how Encapsulation was achieved in the BankAccount class.]
---
+++ client/client.py
@@ -0,0 +1,64 @@
+""" 
+Description: This represents banking client with validation for client number, names and email.
+"""
+
+__author__ = "Komalpreet Kaur"
+__version__ = "1.0.0"
+
+class Client:
+    """Represents a banking client."""
+
+def __init__(self, client_number: int, first_name: str, last_name: str, email_address: str):
+    
+    if isinstance(client_number, int):
+            self.__client_number = client_number
+    else:
+        raise ValueError("Client number must be an integer.")
+    
+    
+    if len(first_name.strip()) > 0:
+            self.__first_name = first_name
+    else:
+        raise ValueError("First name cannot be blank.")
+    
+    if len(last_name.strip()) > 0:
+            self.__last_name = last_name
+    else:
+        raise ValueError("Last name cannot be blank.")
+    
+    
+    try:
+        validate_email(email_address)
+        self.__email_address = email_address
+    except EmailNotValidError:
+        raise ValueError("Invalid email address provided.")
+
+    
+    @property
+    def client_number(self) -> int:
+        return self.__client_number
+    
+    @property
+    def first_name(self) -> str:
+        return self.__first_name
+    
+    @property
+    def last_name(self) -> str:
+        return self.__last_name
+    
+    @property
+    def email_address(self) -> str:
+        return self.__email_address
+    
+    def __str__(self) -> str:
+        return (
+            f"Client Number: {self.__client_number}"
+            f"\nFirst Name: {self.__first_name}"
+            f"\nLast Name: {self.__last_name}"
+            f"\nEmail Address: {self.__email_address}"
+        )
+
+    
+
+
+    
