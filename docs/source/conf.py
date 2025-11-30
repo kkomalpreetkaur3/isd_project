f158cf84eebe4281044bee7503eb8a3c9f610da5
komal Aulakh <kkomalpreetkaur@academic.rrc.ca>
2025-11-30 11:59:09 -0600

    Run sphinx-quickstart and added project and author name

---
+++ docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+# -- Path setup --------------------------------------------------------------
+import os
+import sys
+
+# Add project root directory to Python path
+# (so autodoc can find your modules)
+
+
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'PiXELL-River Financial System'
+copyright = '2025, Komalpreet Kaur'
+author = 'Komalpreet Kaur'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc'
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
